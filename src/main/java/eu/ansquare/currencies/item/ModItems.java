967110788fbf96d14fbd46571f8797ddaf3bfaa9
farpo <77583105+farpo@users.noreply.github.com>
2023-12-05 17:15:50 +0100

    Wallet variants

--- src/main/java/eu/ansquare/currencies/item/ModItems.java
+++ src/main/java/eu/ansquare/currencies/item/ModItems.java
@@ -18,6 +18,22 @@ public class ModItems {
 	private static final Map<Item, Identifier> ITEMS = new LinkedHashMap<>();
 	public static final Item TEST_COIN = createItem("test_coin", new CurrencyItem(new QuiltItemSettings()), ModItemGroups.CURRENCIES);
 	public static final Item LEATHER_WALLET = createItem("leather_wallet", new WalletItem(new QuiltItemSettings().maxCount(1)), ModItemGroups.WALLETS);
+	public static final Item BLUE_WALLET = createItem("blue_wool_wallet", new WalletItem(new QuiltItemSettings().maxCount(1)), ModItemGroups.WALLETS);
+	public static final Item RED_WALLET = createItem("red_wool_wallet", new WalletItem(new QuiltItemSettings().maxCount(1)), ModItemGroups.WALLETS);
+	public static final Item ORANGE_WALLET = createItem("orange_wool_wallet", new WalletItem(new QuiltItemSettings().maxCount(1)), ModItemGroups.WALLETS);
+	public static final Item YELLOW_WALLET = createItem("yellow_wool_wallet", new WalletItem(new QuiltItemSettings().maxCount(1)), ModItemGroups.WALLETS);
+	public static final Item MAGENTA_WALLET = createItem("magenta_wool_wallet", new WalletItem(new QuiltItemSettings().maxCount(1)), ModItemGroups.WALLETS);
+	public static final Item PURPLE_WALLET = createItem("purple_wool_wallet", new WalletItem(new QuiltItemSettings().maxCount(1)), ModItemGroups.WALLETS);
+	public static final Item PINK_WALLET = createItem("pink_wool_wallet", new WalletItem(new QuiltItemSettings().maxCount(1)), ModItemGroups.WALLETS);
+	public static final Item LIME_WALLET = createItem("lime_wool_wallet", new WalletItem(new QuiltItemSettings().maxCount(1)), ModItemGroups.WALLETS);
+	public static final Item GREEN_WALLET = createItem("green_wool_wallet", new WalletItem(new QuiltItemSettings().maxCount(1)), ModItemGroups.WALLETS);
+	public static final Item BROWN_WALLET = createItem("brown_wool_wallet", new WalletItem(new QuiltItemSettings().maxCount(1)), ModItemGroups.WALLETS);
+	public static final Item CYAN_WALLET = createItem("cyan_wool_wallet", new WalletItem(new QuiltItemSettings().maxCount(1)), ModItemGroups.WALLETS);
+	public static final Item LIGHT_BLUE_WALLET = createItem("light_blue_wool_wallet", new WalletItem(new QuiltItemSettings().maxCount(1)), ModItemGroups.WALLETS);
+	public static final Item BLACK_WALLET = createItem("black_wool_wallet", new WalletItem(new QuiltItemSettings().maxCount(1)), ModItemGroups.WALLETS);
+	public static final Item GRAY_WALLET = createItem("gray_wool_wallet", new WalletItem(new QuiltItemSettings().maxCount(1)), ModItemGroups.WALLETS);
+	public static final Item LIGHT_GRAY_WALLET = createItem("light_gray_wool_wallet", new WalletItem(new QuiltItemSettings().maxCount(1)), ModItemGroups.WALLETS);
+	public static final Item WHITE_WALLET = createItem("white_wool_wallet", new WalletItem(new QuiltItemSettings().maxCount(1)), ModItemGroups.WALLETS);
 	private static <T extends Item> T createItem(String name, T item, RegistryKey<ItemGroup> itemGroup){
 		ITEMS.put(item, Currencies.id(name));
 		ItemGroupEvents.modifyEntriesEvent(itemGroup).register(content -> {
